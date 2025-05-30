bb0b997f0e5377b6a95704c65d458e3700008d2d
yamachu <yamachu.dev@gmail.com>
2025-05-30 19:58:59 +0900

    Google認証フロー導線の追加

--- functions/gomeet_function.ts
+++ functions/gomeet_function.ts
@@ -1,5 +1,12 @@
 import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 
+// Google認証用URL生成関数（ダミー）
+function getGoogleAuthUrl(user_id: string): string {
+  // TODO: Google OAuthクライアント情報を用いて認証URLを生成する
+  // credential.json等は後で用意
+  return `https://accounts.google.com/o/oauth2/v2/auth?client_id=TODO_CLIENT_ID&redirect_uri=TODO_REDIRECT_URI&response_type=code&scope=calendar&state=${user_id}`;
+}
+
 // Function定義
 export const GomeetFunctionDefinition = DefineFunction({
   callback_id: "gomeet_function",
@@ -26,11 +33,13 @@ export const GomeetFunctionDefinition = DefineFunction({
 export default SlackFunction(
   GomeetFunctionDefinition,
   ({ inputs }) => {
-    // ここでDatastoreからトークン有無を判定する処理などを今後追加
+    // TODO: Datastoreからinputs.user_idのトークン有無を判定する処理を追加
+    // 今は常に認証案内を返す
+    const authUrl = getGoogleAuthUrl(inputs.user_id);
     return {
       outputs: {
         text:
-          "Google Meetを作成するにはGoogle認証が必要です。後ほど認証フローを案内します。",
+          `Google Meetを作成するにはGoogle認証が必要です。以下のURLから認証を行ってください。\n${authUrl}`,
       },
     };
   },
