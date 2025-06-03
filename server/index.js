30b405581b51060519025422d79b7d2587d85b51
yamachu <yamachu.dev@gmail.com>
2025-06-04 01:01:53 +0900

    とりあえずこいつにcallbackさせると良い

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/oauth2/callback", (c) => {
+  const code = c.req.query("code");
+  const state = c.req.query("state");
+  if (!code) {
+    return c.text("認可コードがありません", 400);
+  }
+  return c.html(`
+    <h1>Google認証が完了しました</h1>
+    <p>下記の認可コードをSlackのGo Meetアプリに貼り付けてください。</p>
+    <pre>${code}</pre>
+    <p>（state: ${state}）</p>
+  `);
+});
+
+serve(app, (info) => {
+  console.log(`Listening on http://localhost:${info.port}`);
+});
